b51f374101ad171e4105d91f92e13e4e37a8e3ad
sammie katt <sammie.katt@gmail.com>
2022-03-22 11:55:10 -0400

    wip: visualize summaries

--- visualize.py
+++ visualize.py
@@ -19,15 +19,42 @@ def main():
     """Function called if run as script"""
     global_parser = argparse.ArgumentParser()
 
+    # global arguments
     global_parser.add_argument(
+        "--save_path", "-o", type=str, help="where to save the file"
+    )
+
+    cmd_parser = global_parser.add_subparsers(dest="cmd")
+
+    mean_of_files_parser = cmd_parser.add_parser("mean_of_files")
+    mean_of_files_parser.add_argument(
+        "files", type=str, nargs="+", help="just list `pkl` files to process"
+    )
+
+    files_as_lines_parser = cmd_parser.add_parser("files_as_lines")
+    files_as_lines_parser.add_argument(
         "files", type=str, nargs="+", help="just list `pkl` files to process"
     )
 
     args = global_parser.parse_args()
-    discounted_returns = [discounted_returns_from_file(f) for f in args.files]
 
-    pd.DataFrame(discounted_returns).mean().plot()
-    plt.show()
+    if args.cmd == "mean_of_files":
+
+        discounted_returns = [discounted_returns_from_file(f) for f in args.files]
+        pd.DataFrame(discounted_returns).mean().plot()
+
+    if args.cmd == "files_as_lines":
+
+        discounted_returns = [discounted_return_from_summary(f) for f in args.files]
+        df = pd.DataFrame(discounted_returns).transpose()
+        df.columns = args.files
+        df.plot()
+
+    # for all functions here: either save or plot
+    if args.save_path:
+        plt.savefig(args.save_path)
+    else:
+        plt.show()
 
 
 def discounted_returns_from_file(f: str) -> List[float]:
@@ -36,11 +63,25 @@ def discounted_returns_from_file(f: str) -> List[float]:
     :param f: file path
     """
 
-    pkl = pickle.load(open(f, "rb"))
+    with open(f, "rb") as open_file:
+        pkl = pickle.load(open_file)
+
     conf, df = pkl["configurations"], pd.DataFrame(pkl["data"])
 
     df["discounted_reward"] = df["reward"] * conf["discount_factor"] ** df["timestep"]
     return df.groupby("episode")["discounted_reward"].sum().tolist()
+
+
+def discounted_return_from_summary(f: str) -> List[float]:
+    """Extracts the returns from summary ``f``
+
+    :param f: file path
+    """
+
+    with open(f, "rb") as open_file:
+        pkl = pickle.load(open_file)
+
+    return pd.DataFrame(pkl["data"])["discounted_reward"].tolist()
 
 
 if __name__ == "__main__":
