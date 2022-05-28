46343444bfd8f6c70b4b56119c69987d5fd5cf90
sammie katt <sammie.katt@gmail.com>
2022-05-27 20:35:34 -0400

    feat: improve summarize

--- summarize.py
+++ summarize.py
@@ -8,14 +8,16 @@ file (`-o`). Here we (hope) to write some (generic) summary to aggregate those
 runs.
 
 """
-
 import argparse
 import pickle
 from collections import defaultdict
 from typing import Any, Dict, List, Tuple
 
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 import yaml
+from PIL import Image
 from yaml.loader import SafeLoader
 
 import wandb
@@ -24,13 +26,20 @@ import wandb
 # Basically each first level is the column that we consider
 # Then we describe how (if) their data is aggregated
 DATA_SUMMARY_CONFIG = {
-    "run": {},
-    "episode": {},
-    "reward": {},
+    "run": {"saved_on_disk": True},
+    "episode": {"saved_on_disk": True},
+    "reward": {"saved_on_disk": True},
+    "discounted_reward": {
+        "agg_over_timesteps": "sum",
+        "agg_over_runs": "mean",
+        "agg_label": "discounted_return",
+        "saved_on_disk": False,
+    },
     "timestep": {
         "agg_over_timesteps": "max",
         "agg_over_runs": "mean",
         "agg_label": "horizon_mean",
+        "saved_on_disk": True,
     },
 }
 
@@ -42,7 +51,23 @@ def main():
     global_parser.add_argument(
         "--save_path", type=str, help="path to save pickle file to"
     )
+
+    # flags to include figures or not
+    global_parser.add_argument(
+        "--aggregate", action="store_true", help="Generatee an aggregated plot"
+    )
+    global_parser.add_argument(
+        "--individual", action="store_true", help="Generate plot with individual lines"
+    )
+    global_parser.add_argument(
+        "--heatmap", action="store_true", help="Generate heatmap plot"
+    )
+    global_parser.add_argument(
+        "--scatterplot", action="store_true", help="Generate scatterplot"
+    )
+
     global_parser.add_argument("--wandb", help="Path to wandb configuration file")
+
     global_parser.add_argument(
         "files", type=str, nargs="+", help="just list `pkl` files to process"
     )
@@ -51,21 +76,43 @@ def main():
 
     print(f"Loading {len(args.files)} files")
     all_data, conf = read_data_to_frame(args.files, DATA_SUMMARY_CONFIG)
+    all_data["discounted_reward"] = (
+        all_data.reward * conf["discount_factor"] ** all_data.timestep
+    )
 
     print("Aggregating results...")
     aggregated_data = aggregate_data_frames(
-        all_data, DATA_SUMMARY_CONFIG, conf["discount_factor"]
+        all_data, DATA_SUMMARY_CONFIG
+    )
+    discounted_return_histogram = (
+        (
+            all_data[["discounted_reward", "episode", "run"]]
+            .groupby(["episode", "run"])
+            .sum()
+        )
+        .reset_index()
+        .pivot(index="episode", columns="run", values="discounted_reward")
     )
 
-    if args.save_path:
-        print(f"Saving data to {args.save_path}")
+    print("Generating plots...")
+    figs: Dict[str, plt.Figure] = generate_figures(
+        all_data, discounted_return_histogram, args
+    )
 
-        with open(args.save_path, "wb") as save_file:
+    # save plots
+    if args.save_path:
+        print(f"Saving data (and figures) to {args.save_path}")
+
+        with open(args.save_path + ".pkl", "wb") as save_file:
             pickle.dump(
                 {"configurations": conf, "data": aggregated_data},
                 save_file,
             )
 
+        for l, f in figs.items():
+            f.savefig(args.save_path + "_" + l)
+
+    # log to wandb
     if args.wandb:
         print("Logging to wandb")
 
@@ -74,7 +121,18 @@ def main():
             conf.update(wandb_conf)
             wandb.init(config=conf, **wandb_conf)
 
-        report_to_wandb(aggregated_data, DATA_SUMMARY_CONFIG)
+        log_to_wandb(aggregated_data, discounted_return_histogram, DATA_SUMMARY_CONFIG)
+
+        wandb.log(
+            {
+                name: wandb.Image(
+                    Image.frombytes(
+                        "RGB", fig.canvas.get_width_height(), fig.canvas.tostring_rgb()
+                    )
+                )
+                for name, fig in figs.items()
+            }
+        )
 
 
 def read_data_to_frame(files: List[str], conf) -> Tuple[pd.DataFrame, Dict[str, Any]]:
@@ -82,6 +140,7 @@ def read_data_to_frame(files: List[str], conf) -> Tuple[pd.DataFrame, Dict[str, 
 
     :param conf: see `DATA_SUMMARY_CONFIG`
     """
+    columns = [k for k, c in conf.items() if c["saved_on_disk"]]
 
     confs: List[Dict[str, Any]] = []
     dfs: List[pd.DataFrame] = []
@@ -94,7 +153,7 @@ def read_data_to_frame(files: List[str], conf) -> Tuple[pd.DataFrame, Dict[str, 
 
             # make sure there is an 'run'
             pkl["data"]["run"] = i
-            dfs.append(pkl["data"][conf.keys()])
+            dfs.append(pkl["data"][columns])
 
     # combine list (conf/dataframe) into single entities
     conf = combine_configurations(confs)
@@ -104,7 +163,7 @@ def read_data_to_frame(files: List[str], conf) -> Tuple[pd.DataFrame, Dict[str, 
 
 
 def aggregate_data_frames(
-    df: pd.DataFrame, conf, discount_factor: float
+    df: pd.DataFrame, conf
 ) -> pd.DataFrame:
     """Here we collect the important data points from data frames ``df``
 
@@ -116,15 +175,11 @@ def aggregate_data_frames(
     # use only relevant data (hopefully these were already trimmed like this)
     df = df[conf.keys()]
 
-    # add things we care about
-    df["discounted_reward"] = df.reward * discount_factor**df.timestep
-
     # here we aggregate per run and episode, integrating out time-steps, according to ``conf``
     per_run_and_episode = (
         df.groupby(["run", "episode"])
         .agg(
             {
-                "discounted_reward": "sum",
                 **{
                     c: data["agg_over_timesteps"]
                     for c, data in conf.items()
@@ -138,7 +193,6 @@ def aggregate_data_frames(
     # here we aggregate over all episodes, integrating out runs, according to ``conf``
     return per_run_and_episode.groupby("episode").agg(
         {
-            "discounted_reward": "mean",
             **{
                 c: data["agg_over_runs"]
                 for c, data in conf.items()
@@ -171,8 +225,10 @@ def combine_configurations(confs: List[Dict[str, Any]]) -> Dict[str, Any]:
     return {k: v.pop() if len(v) == 1 else list(v) for k, v in aggregated_sets.items()}
 
 
-def report_to_wandb(df: pd.DataFrame, conf):
-    """Log some metrics in ``df`` to wandb
+def log_to_wandb(
+    aggregated_data: pd.DataFrame, discounted_return_histogram: pd.DataFrame, conf
+):
+    """Log some metrics in ``aggregated_data`` to wandb
 
     Assumes wandb has been notified/logged into
 
@@ -181,18 +237,101 @@ def report_to_wandb(df: pd.DataFrame, conf):
 
     :param conf: see `DATA_SUMMARY_CONFIG`
     """
-    for ep in range(len(df)):
+    for ep in range(len(aggregated_data)):
 
         wandb.log(
             {
-                "discounted_return_mean": df.loc[ep]["discounted_reward"],
                 **{
-                    data["agg_label"]: df.loc[ep][c]
+                    data["agg_label"]: aggregated_data.loc[ep][c]
                     for c, data in conf.items()
                     if "agg_label" in data
                 },
+                "discounted_return_histogram": wandb.Histogram(
+                    discounted_return_histogram.loc[ep]
+                ),
             }
         )
+
+
+def generate_figures(
+    all_data: pd.DataFrame, discounted_return_histogram: pd.DataFrame, args
+) -> Dict[str, plt.Figure]:
+    """Returns a map of matplotlib figures!
+
+    :params all_data: is the dataframe containing _all_ data (surprise!)
+    :return: a dictionary containing figures, each with a label
+    """
+    df = discounted_return_histogram
+    # to be returned
+    figs: Dict[str, plt.Figure] = {}
+
+    # Totally super obvious way of setting the alpha here. Like, why wouldn't you come up with this formula?
+    # I decided it must be some function of the number of runs...
+    # Why this function? I plotted it and looked nice.
+    alpha = df.shape[1] ** -0.75
+
+    if args.individual:
+        f, ax = plt.subplots()
+        figs["individual"] = f
+        ax.plot(df.rolling(len(df) // 25).mean(), color="blue", label=None, alpha=alpha)
+
+    if args.aggregate:
+        f, ax = plt.subplots()
+        figs["aggregate"] = f
+
+        major_percentiles = [0.25, 0.50, 0.75]
+        minor_percentiles = [0.2, 0.4, 0.6, 0.8]
+
+        df.quantile(major_percentiles, axis="columns").T.plot(
+            ax=ax, color="black", label=[str(i) + "%" for i in major_percentiles]
+        )
+        df.quantile(minor_percentiles, axis="columns").T.plot(
+            ax=ax, color="grey", label=[str(i) + "%" for i in minor_percentiles]
+        )
+
+        df.max(axis="columns").plot(ax=ax, label="max")
+        mean = df.mean(axis="columns")
+        mean.plot(ax=ax, label="mean", color="green")
+        stder = df.sem(axis="columns")
+        ax.fill_between(
+            range(len(df)), mean - stder, mean + stder, alpha=0.2, color="green"
+        )
+
+        ax.legend()
+
+    if args.heatmap:
+        f, _ = plt.subplots()
+        figs["heatmap"] = f
+
+        min_range = df.min().min() - 0.01
+        max_range = df.max().max() + 0.01
+
+        bins = pd.DataFrame(
+            [
+                pd.cut(
+                    df.T[i],
+                    pd.interval_range(min_range, max_range, 10),
+                ).value_counts(sort=False)[::-1]
+                for i in range(len(df))
+            ]
+        )
+        sns.heatmap(bins.transpose(), cmap="Greens")
+
+    if args.scatterplot:
+        f, ax = plt.subplots()
+        figs["scatterplot"] = f
+
+        all_data.plot(
+            ax=ax,
+            x="episode",
+            y="discounted_reward",
+            kind="scatter",
+            alpha=alpha,
+            edgecolor="none",
+            color="black",
+        )
+
+    return figs
 
 
 if __name__ == "__main__":
